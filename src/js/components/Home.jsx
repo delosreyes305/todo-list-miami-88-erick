197253acf35753a8b638ebfb0bf3051001f60f3c
Erick de los Reyes <delosreyeserick90@gmail.com>
2026-02-24 02:22:28 +0000

    API Conected

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,20 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Todo } from "./Todo";
 
 //create your first component
 const Home = () => {
-  //value={inputValue}
-  //onChange={(event) => setInputValue(event.target.value)}
-  //onKeyDown={handleInputChange}
 
   const [inputValue, setInputValue] = useState("");
   const [todos, setTodos] = useState([]);
 
-  const addTodo = () => {
-    if (inputValue.trim() === "") return;
-
-    setTodos([...todos, inputValue]);
-    setInputValue("");
+  const addTodo = async () => {
+    if (inputValue.length > 0) {
+      const response = await fetch(
+        "https://playground.4geeks.com/todo/todos/Erick",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            label: inputValue,
+            is_done: false,
+          }),
+        },
+      );
+      const data = await response.json();
+      getTodos();
+      setInputValue("");
+    }
   };
 
   const handleKeyDown = (event) => {
@@ -23,32 +34,70 @@ const Home = () => {
     }
   };
 
+  const getTodos = async () => {
+    const response = await fetch(
+      "https://playground.4geeks.com/todo/users/Erick",
+    );
+    if (!response.ok) {
+      console.log("Something went wrong!");
+      return;
+    }
+    const data = await response.json();
+    console.log(data);
+    setTodos(data.todos);
+    return data;
+  };
+
+  const createUser = async () => {
+    const response = await fetch(
+      "https://playground.4geeks.com/todo/users/Erick",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        //body: JSON.stringify({
+
+        //})
+      },
+    );
+    if (!response.ok) {
+      getTodos();
+      return;
+    }
+    const data = await response.json();
+    return data;
+  };
+
+  useEffect(() => {
+    createUser().then(() => getTodos());
+  }, []);
+
   return (
-
-
     <div className="text-center m-3">
       <h1>Todo List</h1>
-       <input
-            className="form-control w-50 mx-auto"
-            type="text"
-            placeholder="Add a new task"
-            value={inputValue}
-            onChange={(event) => setInputValue(event.target.value)}
-              onKeyDown={handleKeyDown}
-            
-          />
+      <input
+        className="form-control w-50 mx-auto"
+        type="text"
+        placeholder="Add a new task"
+        value={inputValue}
+        onChange={(event) => setInputValue(event.target.value)}
+        onKeyDown={handleKeyDown}
+      />
       <button className="btn btn-success m-3" onClick={() => addTodo()}>
         Add Task
       </button>
 
       {todos.length !== 0 ? (
-        todos.map((label, index) => (
+        todos.map((todo, index) => (
           <Todo
-            todoValue={label}
+            todoValue={todo.label}
             setTodos={setTodos}
             todos={todos}
             index={index}
-            key={index}
+            key={todo.id}
+            id={todo.id}
+            getTodos={getTodos}
           />
         ))
       ) : (
@@ -56,11 +105,11 @@ const Home = () => {
       )}
       <div className="container d-flex justify-content-center">
         <div className="text-center">
-        <h5 className="ms-50">
-          {todos.length === 1
-            ? `${todos.length} item left!`
-            : `${todos.length} items left!`}
-        </h5>
+          <h5 className="ms-50">
+            {todos.length === 1
+              ? `${todos.length} item left!`
+              : `${todos.length} items left!`}
+          </h5>
         </div>
       </div>
 
--- src/js/components/Todo.jsx
+++ src/js/components/Todo.jsx
@@ -1,34 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 
-export const Todo = ({ index, todos, todoValue, setTodos }) => {
+export const Todo = ({ id, getTodos, todos, todoValue, setTodos }) => {
   const [isHovered, setIsHovered] = useState(false);
 
-  const deleteTodos = () => {
-    console.log("Todo to be deleted: ", index);
-    const newTodos = [...todos];
-    newTodos.splice(index, 1);
-    setTodos(newTodos);
+  const deleteTodos = async() => {
+    const response = await fetch(
+      "https://playground.4geeks.com/todo/todos/" + id,
+      {
+        method: "DELETE"
+      }
+    );
+    getTodos();
+    //console.log("Todo to be deleted: ", index);
+    //const newTodos = [...todos];
+    //newTodos.splice(index, 1);
+    //setTodos(newTodos);
   };
 
   return (
     <div className="container">
-      
       <div
         className="row col-8 mx-auto p-2 border"
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
       >
-        <div className="col-1"><i class="fa-solid fa-thumbtack"></i></div>
+        <div className="col-1">
+          <i className="fa-solid fa-thumbtack"></i>
+        </div>
         <div className="col-10">
           <h2>{todoValue}</h2>
-         
         </div>
         <div className="col-1">
           {isHovered ? (
             <span className="text-danger" onClick={() => deleteTodos()}>
               {" "}
-              <i className="fa-solid me-5 fa-circle-xmark fa-2lg" style={{color: "rgb(212, 36, 36)"}}></i>
+              <i
+                className="fa-solid me-5 fa-circle-xmark fa-2lg"
+                style={{ color: "rgb(212, 36, 36)" }}
+              ></i>
             </span>
           ) : null}
         </div>
